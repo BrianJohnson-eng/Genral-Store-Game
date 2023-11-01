c2c833ff46e495840609a4708023316203529c72
Snakebribri <146724003+Snakebribri@users.noreply.github.com>
2023-11-01 12:57:45 +0000

    altered game code

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,18 @@
-﻿class Program
+﻿using System.Formats.Asn1;
+
+internal class Program
 {
-    static void Main()
+    private static void Main(string[] args)
     {
         int playerCoins = 6000;
-        
+
         int shirtPrice = 2500;
         int helmetPrice = 3000;
         int foodPrice = 500;
         int presentPrice = 1000;
+        int ShoesPrice = 1500;
+
+
 
         Console.WriteLine("------------------------------");
         Console.WriteLine("Welcome to the general store!");
@@ -15,38 +20,74 @@
 
         Console.WriteLine("");
 
-        Console.WriteLine("What would you like to purchase?");
+        while(playerCoins > 0)
 
-        Console.WriteLine("A: A nice shirt.");
-        Console.WriteLine("B: A protective helmet.");
-        Console.WriteLine("C: Some food.");
-        Console.WriteLine("D: A present.");
+        {
+            Console.WriteLine("What would you like to purchase?");
 
-        Console.WriteLine("");
+            Console.WriteLine("A: A nice shirt.");
+            Console.WriteLine("B: A protective helmet.");
+            Console.WriteLine("C: Some food.");
+            Console.WriteLine("D: A present.");
+            Console.WriteLine("E: A pair of Shoes.");
 
-        Console.WriteLine("Enter the letter for your purchase: ");
-        string purchaseLetter = Console.ReadLine();
+            Console.WriteLine("");
 
-        Console.WriteLine("");
+            Console.WriteLine("Enter the letter for your purchase: ");
+            string? purchaseLetter = Console.ReadLine();
 
-        switch (purchaseLetter) {
-            case "A":
-                playerCoins = playerCoins - shirtPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
-                break;
-            case "B":
-                playerCoins = playerCoins - helmetPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
-                break;
-            case "C":
-                playerCoins = playerCoins - foodPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
-                break;
-            case "D":
-                playerCoins = playerCoins - presentPrice;
-                Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
-                break;
-        }
+            Console.WriteLine("");
+
+
+            switch (purchaseLetter)
+            {
+            
+                case "A":
+                    playerCoins = playerCoins - shirtPrice;
+                    if (playerCoins > 0){
+                    Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                    }
+                    else Console.WriteLine($"Thanks for your purchase, you have no coins remaining");
+                    break;
+
+                case "B":
+                    playerCoins = playerCoins - helmetPrice;
+                    if(playerCoins > 0){
+                    Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                    }
+                    else Console.WriteLine($"Thanks for your purchase, you have no coins remaining");
+                    break;
+
+                case "C":
+                    playerCoins = playerCoins - foodPrice;
+                    if (playerCoins > 0){
+                    Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                    }
+                    else Console.WriteLine($"Thanks for your purchase, you have no coins remaining");
+                    break;
+
+                case "D":
+                    playerCoins = playerCoins - presentPrice;
+                    if (playerCoins > 0){
+                    Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                    }
+                    else Console.WriteLine($"Thanks for your purchase, you have no coins remaining");
+                    break;
+                    
+                case "E":
+                    playerCoins = playerCoins - ShoesPrice;
+                   if (playerCoins > 0) {
+                    Console.WriteLine($"Thanks for your purchase! You have {playerCoins} coins remaining.");
+                   }
+                    else Console.WriteLine($"Thanks for your purchase, you have no coins remaining");
+                    break;
+
+                    default:
+                    Console.WriteLine("Thank you for your purchase.");
+                    break;
+           
+            }
+                   
+        }   
     }
 }
-
